7c9ff4815b31699e5d1c34f568ac24e9cb97eb49
dartagnan sams <dartagnansams1@icloud.com>
2021-09-19 23:57:34 -0400

    moved to flask-mail

--- main.py
+++ main.py
@@ -10,12 +10,30 @@ from flask_socketio import (
     leave_room,
 )  # replaces post requests
 
-from send_email import main
+# from send_email import sendEmail
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "hello"  # use session to save personal data to so user doesnt have to log in over and over
 socketio = SocketIO(app)  # init the socket connection
+
+from flask_mail import Mail, Message
+# from main import app
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'dartagnansamsd@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Dartagnan19@'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+mail = Mail(app)
+
+def sendEmail(subject,body,sender,recipient):
+    msg = Message(subject, sender = sender, recipients = [recipient])
+    msg.body = body
+    mail.send(msg)
+    print("email sent")
 
 
 @app.route("/")
@@ -26,7 +44,8 @@ def index():
 @socketio.on("contact")
 def contact(msg):
     print(msg)
-    main(msg["name"],msg["email"],msg["message"])
+    # main(msg["name"],msg["email"],msg["message"])
+    sendEmail(msg["name"],msg["message"],msg["email"],"godofanime72@gmail.com")
 
 if __name__ == "__main__":
     socketio.run(app,debug=True)
--- send_email.py
+++ send_email.py
@@ -1,53 +1,72 @@
-import smtplib
-
-def main(sender,sender_email,message):
-    login = "dartagnansamsd@gmail.com"
-    rec = "godofanime72@gmail.com"
-    password = "Dartagnan19@"
-    # message = "hello world"
-    # creates SMTP session
-    s = smtplib.SMTP_SSL('smtp.googlemail.com', 465)
-    s.ehlo()
-    s.starttls()
-    # s.ehlo()
-    
-    # Authentication
-    # try:
-    s.login(login, password)
-    # except:
-    #     print("Login Credentials is wrong.")
-
-    # message to be sent
-    message = f"""
-        From : {sender}-{sender_email}
-        body: {message}
-    """
-    
-    # sending the mail
-    try:
-        s.sendmail(sender, rec, message)
-    except:
-        print("Email failed to send")
-    
-    # terminating the session
-    s.quit()
-
-
-
-if __name__ == "__main__":
-    main()
-
-
-
-
 # import smtplib
 
-sender = "dartagnansamsd@gmail.com"
-rec = "godofanime72@gmail.com"
-password = "Dartagnan19@"
-message = "hello world"
+# def main(sender,sender_email,message):
+#     login = "dartagnansamsd@gmail.com"
+#     rec = "godofanime72@gmail.com"
+#     password = "Dartagnan19@"
+#     # message = "hello world"
+#     # creates SMTP session
+#     s = smtplib.SMTP_SSL('smtp.googlemail.com', 465)
+#     s.ehlo()
+#     s.starttls()
+#     # s.ehlo()
+    
+#     # Authentication
+#     # try:
+#     s.login(login, password)
+#     # except:
+#     #     print("Login Credentials is wrong.")
 
-# server = smtplib.SMTP("smtp.gmail.com",587)
-# server.starttls()
-# server.login(sender,password)
-# server.sendmail(sender,rec,message)
+#     # message to be sent
+#     message = f"""
+#         From : {sender}-{sender_email}
+#         body: {message}
+#     """
+    
+#     # sending the mail
+#     try:
+#         s.sendmail(sender, rec, message)
+#     except:
+#         print("Email failed to send")
+    
+#     # terminating the session
+#     s.quit()
+
+
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+
+# # import smtplib
+
+# sender = "dartagnansamsd@gmail.com"
+# rec = "godofanime72@gmail.com"
+# password = "Dartagnan19@"
+# message = "hello world"
+
+# # server = smtplib.SMTP("smtp.gmail.com",587)
+# # server.starttls()
+# # server.login(sender,password)
+# # server.sendmail(sender,rec,message)
+
+
+from flask_mail import Mail, Message
+# from main import app
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'yourId@gmail.com'
+app.config['MAIL_PASSWORD'] = '*****'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+mail = Mail(app)
+
+def sendEmail(body,sender,recipient):
+    msg = Message(body, sender = sender, recipient = recipient)
+    msg.body = "This is the email body"
+    mail.send(msg)
+    print("email sent")
