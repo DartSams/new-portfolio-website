973994b0b849a14327c3191fe7d7599a5a93c357
dartagnan sams <dartagnansams1@icloud.com>
2021-09-23 09:30:57 -0400

    connected front end data to yahoo mail smtp

--- main.py
+++ main.py
@@ -9,31 +9,12 @@ from flask_socketio import (
     join_room,
     leave_room,
 )  # replaces post requests
-
-# from send_email import sendEmail
+import smtplib
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "hello"  # use session to save personal data to so user doesnt have to log in over and over
 socketio = SocketIO(app)  # init the socket connection
-
-from flask_mail import Mail, Message
-# from main import app
-
-app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'dartagnansamsd@gmail.com'
-app.config['MAIL_PASSWORD'] = 'Dartagnan19@'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
-
-mail = Mail(app)
-
-def sendEmail(subject,body,sender,recipient):
-    msg = Message(subject, sender = sender, recipients = [recipient])
-    msg.body = body
-    mail.send(msg)
-    print("email sent")
 
 
 @app.route("/")
@@ -41,24 +22,21 @@ def index():
 
     return render_template("index.html")
 
-import sendgrid
-import os
-from sendgrid.helpers.mail import *
-@socketio.on("contact")
-def contact(msgj):
-    print(msgj)
 
-    import smtplib
+@socketio.on("contact")
+def contact(msg):
+    print(msg)
+    
     email_address = 'dartagnansams1@yahoo.com'     # add email address here
-    Subject = 'Subject: So long...\n\n'
-    content = ' Dear Test, \n This is a test message.\n\n ' 
-    footer = '- Test'    # add test footer 
+    Subject = 'Subject: From portfolio website'
+    content = f'{msg["message"]}' 
+    footer = '- Portfolio Website'    # add test footer 
     # passcode = 'Python343419@'        # add passcode here
-    passcode = 'xghivoqldwfryrpu'
+    passcode = os.environ.get("yahoo_api_key")
     conn = smtplib.SMTP_SSL('smtp.mail.yahoo.com', 465) 
     conn.ehlo()
     conn.login(email_address, passcode)
-    conn.sendmail(email_address,
+    conn.sendmail(msg['email'],
                 email_address,
                 Subject + content + footer)
     conn.quit()
