fdf5c46366e33c11f3cdfc3870d566a474764fe1
dartsams <dartagnansams1@icloud.com>
2023-01-17 02:12:11 -0500

    updated resume,moved website to replit,next need to redesign and update to fit new projects

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from replit import web
 from flask import Flask, render_template, redirect, request, flash, session, url_for
 from werkzeug.utils import secure_filename  # upload images
 import os
@@ -13,7 +14,7 @@ import smtplib,ssl
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "hello" 
-socketio = SocketIO(app)  # init the socket connection
+# socketio = SocketIO(app)  # init the socket connection
 
 
 @app.route("/")
@@ -21,19 +22,20 @@ def index():
     return render_template("index2.html")
 
 
-@socketio.on("send_email")
-def send_email(msg):
-    print(msg)
+# @socketio.on("send_email")
+# def send_email(msg):
+#     print(msg)
     
-    email_address = 'dartagnansams1@yahoo.com'
-    Subject = 'Subject: From portfolio website\n\n'
-    content = f'From:{msg["name"]}\n By:{msg["email"]}\n\n{msg["message"]}' 
-    footer = '- Portfolio Website' 
-    passcode = "hknjyqdnuooiiegy" #IMPORTANT this is yahoo api token needed to login
-    conn = smtplib.SMTP_SSL('smtp.mail.yahoo.com', 465) 
-    conn.ehlo()
-    conn.login(email_address, passcode)
-    conn.sendmail(email_address,email_address,Subject + content + footer)
+#     email_address = 'dartagnansams1@yahoo.com'
+#     Subject = 'Subject: From portfolio website\n\n'
+#     content = f'From:{msg["name"]}\n By:{msg["email"]}\n\n{msg["message"]}' 
+#     footer = '- Portfolio Website' 
+#     passcode = "hknjyqdnuooiiegy" #IMPORTANT this is yahoo api token needed to login
+#     conn = smtplib.SMTP_SSL('smtp.mail.yahoo.com', 465) 
+#     conn.ehlo()
+#     conn.login(email_address, passcode)
+#     conn.sendmail(email_address,email_address,Subject + content + footer)
 
-if __name__ == "__main__":
-    socketio.run(app,debug=True,port=8000)
+web.run(app)
+# if __name__ == "__main__":
+#     socketio.run(app,debug=True,port=8000)
